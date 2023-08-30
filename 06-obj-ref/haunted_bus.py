5414d8829004db5a067b5ff254a787d88fff03ca
HuangMiao <huangmiao@oschina.cn>
2023-08-30 18:53:38 +0800

    update haunted_bus.py

--- 06-obj-ref/haunted_bus.py
+++ 06-obj-ref/haunted_bus.py
@@ -23,7 +23,7 @@ True
 
 
 >>> dir(HauntedBus.__init__)  # doctest: +ELLIPSIS
-['__annotations__', '__call__', ..., '__defaults__', ...]
+['__annotations__', ..., '__defaults__', ...]
 >>> HauntedBus.__init__.__defaults__
 (['Carrie', 'Dave'],)
 >>> HauntedBus.__init__.__defaults__[0] is bus2.passengers
